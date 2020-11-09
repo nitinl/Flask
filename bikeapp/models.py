203488e5d0f0cbd35a600f1d22ecbfbd9ebbfdb9
bsinoimeri <b.sinoimeri@hotmail.com>
2020-11-09 22:24:05 +0100

    User Authentication

--- bikeapp/models.py
+++ bikeapp/models.py
@@ -1,8 +1,14 @@
 from datetime import datetime
-from bikeapp import db
+from bikeapp import db, login_manager
+from flask_login import UserMixin
+
+# Check the user_id in the current session & load the User object for that id.
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 
-class User(db.Model):
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
