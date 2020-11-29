3f3c0e7d386641b30783b804f9c9b9632c8fa624
bsinoimeri <b.sinoimeri@hotmail.com>
2020-11-29 21:18:10 +0100

    Restructure with Blueprints

--- bikeapp/__init__.py
+++ bikeapp/__init__.py
@@ -1,25 +1,34 @@
-import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
+from bikeapp.config import Config
 
-app = Flask(__name__)
-app.config['SECRET_KEY'] = '57411af3803d40de9186755d5c9ba1c4'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///bike.db'
-db = SQLAlchemy(app)
-bcrypt = Bcrypt(app)
-login_manager = LoginManager(app)
 
-login_manager.login_view = 'login'
+db = SQLAlchemy()
+bcrypt = Bcrypt()
+login_manager = LoginManager()
+mail = Mail()
+
+login_manager.login_view = 'users.login'
 login_manager.login_message_category = 'info'
-app.config['MAIL_SERVER'] = 'smtp.gmail.com'
-app.config['MAIL_PORT'] =  465
-app.config['MAIL_USE_SSL'] = True
-app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
-app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
 
-mail = Mail(app)
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(Config)
 
-from bikeapp import routes
+    db.init_app(app)
+    bcrypt.init_app(app)
+    login_manager.init_app(app)
+    mail.init_app(app)
+
+    from bikeapp.users.routes import users
+    from bikeapp.posts.routes import posts
+    from bikeapp.main.routes import main
+
+    app.register_blueprint(users)
+    app.register_blueprint(posts)
+    app.register_blueprint(main)
+
+    return app
--- run.py
+++ run.py
@@ -1,4 +1,7 @@
-from bikeapp import app
+from bikeapp import create_app
+
+app = create_app()
+
 
 if __name__ == '__main__':
     app.run(debug=True)
